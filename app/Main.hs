d75dd77dff5e5e9bd905b434f974f6049429c99a
Charles Bloomfield <charlie@wpa146073.wireless.mcgill.ca>
2017-02-20 15:16:43 -0500

    ADD: initial command line args

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,35 @@
 module Main where
 
-import Scanner
+
+import System.FilePath
+import System.Console.ArgParser
+import qualified Parser
+import qualified Pretty
+
+
+data GoLiteOptions = GoLiteOptions
+  { filename :: String }
+  deriving (Show)
+
+
+goLiteOptionsParser :: ParserSpec GoLiteOptions
+goLiteOptionsParser = GoLiteOptions
+  `parsedBy` reqPos "filename" `Descr` "goLite source file with relative file path"
+
 
 main :: IO ()
-main = putStrLn "Hello World!"
+main = do
+  interface <- mkApp goLiteOptionsParser
+  runApp interface compile
+
+
+compile :: GoLiteOptions -> IO ()
+compile options = do
+  text <- readFile goLiteFile
+  case Parser.parse goLiteFile text of
+    Right program -> writeFile prettyFile $ Pretty.pretty program
+    Left errorMsg -> putStrLn errorMsg
+
+  where
+    goLiteFile = filename options
+    prettyFile = replaceExtension goLiteFile ".pretty.go"
