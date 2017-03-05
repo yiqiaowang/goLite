49a0383b68e51ff8084b21e4ead22e59719147e3
Yi Qiao Wang <yiqiao.wang96@gmail.com>
2017-03-05 14:59:43 -0500

    hindent file

--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,33 @@
 module Main where
 
-
-import System.IO(hPutStrLn, stderr)
-import System.FilePath
-import System.Console.ArgParser
+import GoLite (parse)
 import qualified Parser
 import qualified Pretty
-import GoLite(parse)
-
+import System.Console.ArgParser
+import System.FilePath
+import System.IO (hPutStrLn, stderr)
 
 data GoLiteOptions = GoLiteOptions
-  { filename :: String }
-  deriving (Show)
-
+  { filename :: String
+  } deriving (Show)
 
 goLiteOptionsParser :: ParserSpec GoLiteOptions
-goLiteOptionsParser = GoLiteOptions
-  `parsedBy` reqPos "filename" `Descr` "goLite source file with relative file path"
-
-
+goLiteOptionsParser =
+  GoLiteOptions `parsedBy` reqPos "filename" `Descr`
+  "goLite source file with relative file path"
 
 main :: IO ()
 main = do
   interface <- mkApp goLiteOptionsParser
   runApp interface compile
 
-
 compile :: GoLiteOptions -> IO ()
 compile options = do
   text <- readFile goLiteFile
   case parse goLiteFile text of
-    Right program -> writeFile prettyFile $ Pretty.pretty program 0
+    Right program -> do
+      writeFile prettyFile $ Pretty.pretty program 0
     Left errorMsg -> errorWithoutStackTrace $ show errorMsg
-
   where
     goLiteFile = filename options
     prettyFile = replaceExtension goLiteFile ".pretty.go"
