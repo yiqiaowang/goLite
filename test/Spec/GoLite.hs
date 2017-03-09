dac11a337d93415ea2a7d2818438b01ebda59544
Charles Bloomfield <charlielbloomfield@gmail.com>
2017-03-09 12:55:13 -0500

    ADD: test code for type checker

--- test/Spec/GoLite.hs
+++ test/Spec/GoLite.hs
@@ -13,8 +13,14 @@ import GoLite
 type Programs = [(String, String)]
 
 
-spec :: Programs -> Programs -> Programs -> Spec
-spec invalidParser invalidWeeder validSyntax =
+spec
+  :: Programs
+  -> Programs
+  -> Programs
+  -> Programs
+  -> Programs
+  -> Spec
+spec invalidParser invalidWeeder validSyntax invalidTypeChecker validTypeChecker =
   describe "GoLite" $ do
     forM_ invalidParser $ \(file, text) ->
       it ("fails with a parser error : " ++ file) $
@@ -28,8 +34,22 @@ spec invalidParser invalidWeeder validSyntax =
       it ("correctly parses and weeds : " ++ file) $
         GoLite.parse file text `shouldSatisfy` isRight
 
+    forM_ invalidTypeChecker $ \(file, text) ->
+      it ("correctly parses but fails to type check : " ++ file) $
+        case GoLite.parse file text of
+          Right program -> GoLite.typeCheck program `shouldSatisfy` isTypeError
+          Left parseError -> fail $ show parseError
+
+    forM_ validTypeChecker $ \(file, text) ->
+      it ("correctly parses and type checks : " ++ file) $
+        case GoLite.parse file text of
+          Right program -> GoLite.typeCheck program `shouldSatisfy` isRight
+          Left parseError -> fail $ show parseError
+
     where
       isParserError (Left (ParserError _)) = True
       isParserError _ = False
       isWeederError (Left (WeederError _)) = True
       isWeederError _ = False
+      isTypeError (Left (TypeCheckerError _)) = True
+      isTypeError _ = False
