6264f293727f5cb93c41d23387ba026a06519eb9
Charles Bloomfield <charlielbloomfield@gmail.com>
2017-03-10 13:13:17 -0500

    FIX: broken tests

--- programs/valid/typechecker/expressions/binary_comparable_array.go
+++ programs/valid/typechecker/expressions/binary_comparable_array.go
@@ -2,7 +2,7 @@ package expressions
 
 
 func main() {
-  var s1, s2 int[0]
+  var s1, s2 [0]int
 
   var a bool = s1 == s2
   var b bool = s1 != s2
--- programs/valid/typechecker/expressions/binary_comparable_slice.go
+++ programs/valid/typechecker/expressions/binary_comparable_slice.go
@@ -2,7 +2,7 @@ package expressions
 
 
 func main() {
-  var s1, s2 int[]
+  var s1, s2 []int
 
   var a bool = s1 == s2
   var b bool = s1 != s2
