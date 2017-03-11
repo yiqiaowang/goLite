cc3ebeeb905b3694ab3718445a0b7fa8c458d3cb
Yi Qiao Wang <yiqiao.wang96@gmail.com>
2017-03-10 20:25:09 -0500

    Fix: broken tests

--- programs/valid/typechecker/expressions/field_selection.go
+++ programs/valid/typechecker/expressions/field_selection.go
@@ -1,19 +1,19 @@
 package expressions
 
-type point {
+type point struct {
   x, y int
 }
 
-type circle {
+type circle struct {
   center point
   radius int
 }
 
 func main() {
   var c circle
-
   var x int = c.center.x
   var y int = c.center.y
   var p point = c.center
   var r int = c.radius
+  return
 }
