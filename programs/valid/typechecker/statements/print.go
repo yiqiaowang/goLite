5d084bfce00a871862d85adee0359f9db0029345
Yi Qiao Wang <yiqiao.wang96@gmail.com>
2017-03-10 20:18:27 -0500

    Fix: print can't print struct, array or slices

--- programs/valid/typechecker/statements/print.go
+++ programs/valid/typechecker/statements/print.go
@@ -1,17 +1,7 @@
 package statements
 
-type point struct {
-  x, y, int
-}
-
-type point1 point
-
 func main() {
-  var arr [0]int
-  var slice []int
-  var p point
-  var p1 point1
-
   print()
-  print(0, 0.0, true, false, ' ', " ", ` `, arr, slice, p, p1)
+  print(0, 0.0, true, false, ' ', " ", ` `)
+  return
 }
