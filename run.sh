72d72c220659f8e7f97e6c04f9686ff6cc1d0077
Charles Bloomfield <charlielbloomfield@gmail.com>
2017-03-23 15:04:16 -0400

    ADD: execution commands for js file

--- run.sh
+++ run.sh
@@ -2,4 +2,14 @@
 
 args="$@"
 
+# first arg should be filename
+go_lite_file="$1"
+
+# execute compiler
 stack exec golite-exe -- $args -t
+
+# replace goLite file with .js extension
+#js_file="${go_lite_file%.go}.js"
+
+# execute generated code with system npm
+#npm $js_file
