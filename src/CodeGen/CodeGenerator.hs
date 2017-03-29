423021008376d19286b5520abd7bf8c452f65f58
Charles Bloomfield <charlielbloomfield@gmail.com>
2017-03-29 17:29:48 -0400

    REFACTOR: split up code gen

--- src/CodeGen/CodeGenerator.hs
+++ src/CodeGen/CodeGenerator.hs
@@ -10,13 +10,9 @@ import Data.Char(chr)
 import Language
 import CodeGen.Comparison
 import CodeGen.Array
+import CodeGen.Expressions
+import CodeGen.Codeable
 
-
-class Codeable a where
-  code :: a -> Integer -> String
-
-  codeList :: [a] -> Integer -> String
-  codeList ps i = concatMap (`code` i) ps
 
 structList :: ([Identifier], Type) -> Integer -> String -> String
 structList ([], _) i s = ""
@@ -48,13 +44,6 @@ emptyTypeValue (Struct struct) i s =
   concat
     ["{};\n"
     , structPrint struct (i + 1) s]
-
---
-commaSepList :: Codeable a => [a] -> Integer -> String
-commaSepList string i = intercalate ", " (map (`code` i) string)
-
-commaSpaceSepList :: Codeable a => [a] -> Integer -> String
-commaSpaceSepList string i = intercalate ", \" \", " (map (`code` i) string)
 
 varList :: [Identifier] -> (Maybe Type) -> [Expression] -> Integer -> String
 varList [] exprs _ _ = ""
@@ -92,22 +81,6 @@ varList (var:vars) t (expr:exprs) i =
           , code expr i
           , "\n"
           , varList vars t exprs i]
-
---
-spacePrint :: Integer -> String
-spacePrint x = replicate (fromInteger x) '\t'
-
---
-wrapSquare :: String -> String
-wrapSquare s = "[" ++ s ++ "]"
-
---
-wrapSquareList :: Codeable a => [a] -> Integer -> String
-wrapSquareList xs i = concatMap wrapSquare (map (`code` i) xs)
-
---
-dotSepList :: [String] -> String
-dotSepList = intercalate "."
 
 -- Need to do
 instance Codeable Program where
@@ -228,11 +201,6 @@ instance Codeable TypeName where
       , "}\n"]
   code (TypeName _ _) i = ""
 
-instance Codeable Identifier where
-  code (IdOrType s) i = s
-  code (IdArray s xs) i = concat [s, wrapSquareList (map (`code` 0) xs) i]
-  code (IdField xs) i = intercalate "." $ map (`code` i) xs
-
 -- UNUSED!!!!!!!!!!!
 instance Codeable Type where
   code (Alias s) _ = s
@@ -334,10 +302,6 @@ instance Codeable SimpleStmt where
       ]
   code (EmptyStmt) i = ""
 
-instance Codeable FunctionCall where
-  code (FunctionCall ident exprList) i =
-    concat [code ident i, "(", commaSepList exprList i, ")"]
-
 instance Codeable IfStmt where
   code (IfStmt st expr stList (IfStmtCont Nothing)) i =
     concat
@@ -384,92 +348,6 @@ instance Codeable Clause where
       ]
   code (Default stList) i =
     concat [spacePrint i, "default:\n", codeList stList (i + 1)]
-
-instance Codeable Expression where
-  code (Brack expr) i = concat
-    [ "("
-    , code expr i
-    , ")"
-    ]
-  code (Id ident) i = code ident i
-  code (Literal lit) i = code lit i
-  code (Unary op expr) i = concat [code op i, code expr i]
-  code (Binary Equals expr1 expr2) i =
-    "GO_LITE_EQUALS(" ++ code expr1 i ++ ", " ++ code expr2 i ++ ")"
-  code (Binary NotEquals expr1 expr2) i =
-    "GO_LITE_NOT_EQUALS(" ++ code expr1 i ++ ", " ++ code expr2 i ++ ")"
-  code (Binary op expr1 expr2) i = concat
-    [ code expr1 i
-    , " "
-    , code op i
-    , code expr2 i
-    ]
-  code (ExprFuncCall func) i = code func i
-  code (Append ident expr) i =
-    concat ["append(", code ident i, ", ", code expr i, ")"]
-
--- Need to do
-instance Codeable UnaryOp where
-  code Pos _ = "+"
-  code Neg _ = "-"
-  code BoolNot _ = "!"
-  code BitComplement _ = "~"
-
--- Need to do
-instance Codeable BinaryOp where
-  code Or _ = "|| "
-  code And _ = "&& "
-  code Equals _ = "== "
-  code NotEquals _ = "!= "
-  code LThan _ = "< "
-  code LEThan _ = "<= "
-  code GThan _ = "> "
-  code GEThan _ = ">= "
-  code Add _ = "+ "
-  code Sub _ = "- "
-  code Mult _ = "* "
-  code Div _ = "/ "
-  code Mod _ = "% "
-  code BitAnd _ = "& "
-  code BitOr _ = "| "
-  code BitXor _ = "^ "
-  code BitLShift _ = "<< "
-  code BitRShift _ = ">> "
-  code BitClear _ = "& ~"
-
--- Need to do
-instance Codeable BinaryOpEq where
-  code PlusEq _ = "+= "
-  code MinusEq _ = "-= "
-  code MulEq _ = "*= "
-  code DivEq _ = "/= "
-  code ModEq _ = "%= "
-  code BitAndEq _ = "&= "
-  code BitOrEq _ = "|= "
-  code BitXorEq _ = "^= "
-  code BitLShiftEq _ = "<<= "
-  code BitRShiftEq _ = ">>= "
-  code BitClearEq _ = "&= ~"
-
-instance Codeable (Maybe Expression) where
-  code Nothing i = "true"
-  code (Just e) i = code e i
-
-instance Codeable String where
-  code s _ = s
-
-instance Codeable Integer where
-  code int _ = (show int)
-
-instance Codeable Int where
-  code int _ = (show int)
-
-instance Codeable Literal where
-  code (Int' i) _ = show i
-  code (Float64 f) _ = show f
-  code (Rune i) _ = (show . chr . fromIntegral) i
-  code (String s) _ = s
-  code (Raw s) _ = s
 
 class Injectable a where
   inject :: [a] -> SimpleStmt -> [a]
