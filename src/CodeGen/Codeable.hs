75527a0851fbeea9a723a206f205fecc9dc3c210
Charles Bloomfield <charlielbloomfield@gmail.com>
2017-04-02 14:58:51 -0400

    ADD: type info to codegen

--- src/CodeGen/Codeable.hs
+++ src/CodeGen/Codeable.hs
@@ -3,17 +3,18 @@ module CodeGen.Codeable where
 
 import Data.List(intercalate)
 import Language
+import SymbolTable
 
 class Codeable a where
-  code :: a -> Integer -> String
+  code :: a -> Integer -> History -> String
 
-  codeList :: [a] -> Integer -> String
-  codeList ps i = concatMap (`code` i) ps
+  codeList :: [a] -> Integer -> History -> String
+  codeList cs i h = concatMap (\c -> code c i h) cs
 
 
 --
-commaSepList :: Codeable a => [a] -> Integer -> String
-commaSepList string i = intercalate ", " (map (`code` i) string)
+commaSepList :: Codeable a => [a] -> Integer -> History -> String
+commaSepList cs i h = intercalate ", " (map (\c -> code c i h) cs)
 
 --
 spacePrint :: Integer -> String
@@ -24,8 +25,8 @@ wrapSquare :: String -> String
 wrapSquare s = "[" ++ s ++ "]"
 
 --
-wrapSquareList :: Codeable a => [a] -> Integer -> String
-wrapSquareList xs i = concatMap wrapSquare (map (`code` i) xs)
+wrapSquareList :: Codeable a => [a] -> Integer -> History -> String
+wrapSquareList cs i h = concatMap (wrapSquare . (\c -> code c i h)) cs
 
 --
 dotSepList :: [String] -> String
